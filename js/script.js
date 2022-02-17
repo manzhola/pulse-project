9d6c1cea74ce2cde3d566951eb407d1858dd5578
Manzhola <vladimirmanzhola073@gmail.com>
2022-02-17 12:34:44 +0200

    add Tabs and slide-links

--- js/script.js
+++ js/script.js
@@ -5,4 +5,23 @@ $(document).ready(function(){
         prevArrow: '<button type="button" class="slick-prev slick"><img src="icons/chevron-left-solid.svg"></button>',
         nextArrow: '<button type="button" class="slick-next slick"><img src="icons/chevron-right-solid.svg"></button>'
     });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
+
+    function toggleSlider(item) {
+        $(item).each(function (i){
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        })
+    }
+
+    toggleSlider('.catalog-item__back')
+    toggleSlider('.catalog-item__link')
 });
